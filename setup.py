d8a1dec616d65e84dd72d0091586f79a8be2b816
Franz Wöllert <franz.woellert@heidelberg.com>
2022-07-16 12:41:13 +0200

    Use quotations marks for version specififer.

--- setup.py
+++ setup.py
@@ -122,8 +122,8 @@ setup(
     # setup_requires=['pypandoc<1.2.0', 'setuptools-markdown'],
     setup_requires=['setuptools-markdown'],
     install_requires=[
-        "ansible<2.10;python_version<='3.6'"
-        "ansible<5;python_version>='3.8'",
+        "ansible<'2.10';python_version<='3.6'"
+        "ansible<'5';python_version>='3.8'",
         'pytest'],
     cmdclass={
         'test': ToxTestCommand,
